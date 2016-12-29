36dd491f8826f2c88d71de551a7e676c24e33e46
tehZevo <What email?>
2016-12-29 18:24:17 -0500

    Split main file
    - Implemented instruction set modification by using Object.assign
    - Added instruction set and debug parameters to Runner

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # JunkScript
 A futile venture in the world of stack-based golfing languages built in JavaScript.
+
+Runs in node. No dependencies yet.
+
+```
+node junkscript.js "11+"
+```
--- junkscript.js
+++ junkscript.js
@@ -1,132 +1,13 @@
-var f =
-{
-  //push the number 0 onto the stack
-  "0": function(runner)
-  {
-    runner.stack.push(0);
-  },
+var Runner = require("./lib/runner.js");
+var core = require("./lib/instructions/core.js");
 
-  //push the number 1 onto the stack
-  "1": function(runner)
-  {
-    runner.stack.push(1);
-  },
+var instructions = {};
 
-  //push the number 9 onto the stack
-  "9": function(runner)
-  {
-    runner.stack.push(9);
-  },
-
-  //pop two objects off the stack, add them, push result on the stack
-  "+": function(runner)
-  {
-    runner.stack.push(runner.stack.pop() + runner.stack.pop());
-  },
-
-  //copy the last object on the stack
-  ":": function(runner)
-  {
-    runner.stack.push(stack[runner.stack.length - 1]);
-  },
-
-  //copy the last two objects on the stack
-  ";": function(runner)
-  {
-    runner.stack.push(stack[runner.stack.length - 2]);
-    runner.stack.push(stack[runner.stack.length - 2]);
-  },
-
-  //swap the last two objects on the stack
-  "~": function(runner)
-  {
-    var a = runner.stack.pop();
-    var b = runner.stack.pop();
-
-    runner.stack.push(b);
-    runner.stack.push(a);
-  },
-
-  //lookahead on the program and push the function onto the stack (ending with })
-  //provide running context
-  "{": function(runner)
-  {
-    var end = program.indexOf("}");
-    var sub = program.substring(runner.pc + 1, end);
-
-    runner.pc = end;
-
-    function fun()
-    {
-      new Runner(sub, runner.stack).run();
-    }
-
-    runner.stack.push(fun);
-  },
-
-  //pop and execute the last object on the stack (function)
-  "!": function(runner)
-  {
-    runner.stack.pop()();
-  },
-
-  //cycle the stack left once
-  "<": function(runner)
-  {
-    runner.stack.push(runner.stack.shift());
-  },
-
-  //cycle the stack right once
-  ">": function(runner)
-  {
-    runner.stack.unshift(runner.stack.pop());
-  },
-
-  //repeat the function at %2 %1 times
-  "&": function(runner)
-  {
-    var count = runner.stack.pop();
-    var fun = runner.stack.pop();
-
-    for(var i = 0; i < count; i++)
-    {
-      fun();
-    }
-  },
-
-  //take the last %1 elements (other than %1), and store them in an array
-  "[": function(runner)
-  {
-    runner.stack.unshift(runner.stack.pop());
-  },
-}
+Object.assign(instructions, core);
 
 var stack = [];
 var program = process.argv[2];
 
-class Runner
-{
-  constructor(program, stack)
-  {
-    this.program = program;
-    this.pc = 0;
-    this.stack = stack || [];
-  }
-
-  run()
-  {
-    console.log("in");
-    while(this.pc < this.program.length)
-    {
-      console.log(this.pc + ": " + this.program[this.pc]);
-      f[this.program[this.pc]](this);
-
-      this.pc++;
-    }
-    console.log("out");
-  }
-}
-
-new Runner(program, stack).run();
+new Runner(instructions, program, stack, true).run();
 
 console.log(stack);
