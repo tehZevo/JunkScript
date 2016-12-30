8068eb9b46d27acf30427ca4ab68f2975f37743e
tehZevo <What email?>
2016-12-30 17:24:52 -0500

    more functionality
    - string/array related things
    - added a "heap"
    - a few math/control instructions

--- junkscript.js
+++ junkscript.js
@@ -1,7 +1,7 @@
 var Runner = require("./lib/runner.js");
 var Utils = require("./lib/utils.js");
 
-var instructionSets = ["core", "constants", "stack", "array", "math", "io", "macros"];
+var instructionSets = ["core", "constants", "stack", "stringandarray", "math", "io", "macros", "control"];
 
 var instructions = {};
 
@@ -13,12 +13,15 @@ for(var i = 0; i < instructionSets.length; i++)
 }
 
 var stack = [];
+var heap = {};
 var program = process.argv[2];
 
-new Runner(instructions, program, stack, false).run();
+new Runner(instructions, program, stack, heap, false).run();
 
+//do not change stack reference
 console.log(stack);
 
+//print out list of characters used (may not render all correctly)
 //console.log(Object.keys(instructions).sort().join(""));
 
 //console.log(Utils.findMatching("{}}}}", "{", "}", "{", "}"));
